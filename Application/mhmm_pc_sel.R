1488943665b5b0b95497a0e8270b47093e480d90
lruijin <lruijin@vt.edu>
2021-10-15 10:28:20 -0400

    Update mhmm_pc_sel.R

--- Application/mhmm_pc_sel.R
+++ Application/mhmm_pc_sel.R
@@ -18,16 +18,25 @@ get_inits <- function(sample,burnin,inits,hs,K,Km){
   P1 <- inits[sample$pidx$P1]
   return(c(pi,P0,tau,beta,Sigma,invsigma,P1))
 }
+
+# ------ Step 1: read in data ------
 data <- read.csv("../Fmod_imputed.csv",header = T)
 data.na <- na.omit(data)
+# The covariates: age and gender
 X <- read.csv("../age_sex.csv",header=T)
+# GROUP in A1C.csv indicates the study arm each family belongs to
 A1C <- read.csv("../A1C.csv",header = T)
+# The four main manifesto variables
 y_c = list(t(matrix(data.na$CxDFR,nr = 5)),t(matrix(log(data.na$CxPCC),nr = 5)))
 y_p = list(t(matrix(data.na$PxDFR,nr = 5)),t(matrix(log(data.na$PxPCC),nr = 5)))
 #y_c <- lapply(y_c,function(x) (x-mean(x))/sd(x))
 #y_p <- lapply(y_p,function(x) (x-mean(x))/sd(x))
 
-source('../../code/MHMM_v4.R')
+# load in the functions to implement the method
+source('../MHMM_v4.R')
+
+# ------ Step 2: Set up initial values ------
+# 2-class model
 inits.pc_hs2 = c(0.547,0.353,0.014, 
                 34.067,-2.671,3.4925,-0.3385,
                 37.197,-3.9,3.274,-0.055,
@@ -35,6 +44,7 @@ inits.pc_hs2 = c(0.547,0.353,0.014,
                 13.062,0.032,9.663,-0.023,0.032,0.026,0.019,0.01,9.663,0.019,13.259,0.098,-0.023,0.01,0.098,0.029,
                 rep(c(0.154,60.897,0.201,52.557),each=2),
                 0.459,0.012)
+# 3-class model
 inits.pc_hs3 = c(0.056,0.815,
 		0.223,0.057,0.027,0.39,0.005,0.011, 
                  34.102,-0.008,-2.636,3.791,-0.598,-0.04,
@@ -43,7 +53,7 @@ inits.pc_hs3 = c(0.056,0.815,
  		 13.062,0.032,9.663,-0.023,0.032,0.026,0.019,0.01,9.663,0.019,13.259,0.098,-0.023,0.01,0.098,0.029,
                 rep(c(0.154,60.897,0.201,52.557),each=3),
                  0.056,0.077,0.010,0.303,0.005,0.010)
-
+# 4-class model
 inits.pc_hs4 = c(0.139,0.408,0.408,
                  0.275,0.275,0.077,0.017,0.14,0.27,0.017,0.14,0.27,0.006,0.009,0.009, 
                  34.102,-0.004,-0.004,-2.636,3.791,-0.299,-0.299,-0.04,
@@ -53,6 +63,7 @@ inits.pc_hs4 = c(0.139,0.408,0.408,
                  rep(c(0.154,60.897,0.201,52.557),each=4),
                  0.356,0.356,0.103,0.014,0.374,0.259,0.014,0.374,0.259,0.006,0.006,0.006)
 
+# 5-class model
 inits.pc_hs5 = c(0.139,0.408,0.408,0.023,
                  0.275,0.275,0.039,0.039,0.017,0.14,0.135,0.135,0.017,0.14,0.135,0.135,0.006,0.009,0.009,0.489,0.006,0.009,0.009,0.489,
                  34.102,-0.004,-0.004,-1.318,-1.318,3.791,-0.299,-0.299,-0.02,-0.02,
@@ -61,8 +72,19 @@ inits.pc_hs5 = c(0.139,0.408,0.408,0.023,
                  13.062,0.032,9.663,-0.023,0.032,0.026,0.019,0.01,9.663,0.019,13.259,0.098,-0.023,0.01,0.098,0.029,
                  rep(c(0.154,60.897,0.201,52.557),each=5),
                  0.356,0.356,0.052,0.051,0.014,0.374,0.129,0.130,0.014,0.374,0.13,0.129,0.006,0.006,0.006,0.491,0.006,0.006,0.006,0.491)
+# set the number of hidden states as 2, 3, 4, or 5
 hs = 3
 print(paste("MHMM: The number of hidden states is:",hs))
+
+# ------- Step 3: obtain posterior samples from MHMM model: ------
+# The following function fitts MHMM model by setting fitRx as (F,T).
+# Thus, transition probabilities are arm-specific.
+
+# Detailed arguments to be found in "MHMM_v4.R"
+# Only change "inits." when setting different number of hidden states
+# 	e.g: When hs = 3 set inits. = inits.pc_hs3 in the following function
+# Note: emission.fix is set to be F to allow different emission models for each family member
+
 mhmm <- simulated_mhmm(y = c(y_c,y_p), X = as.matrix(X,ncol=2),inits. = inits.pc_hs3, report1. = 1000, burnin=30000,emission.fix = F,no.random = F,
                        nsim. = 40000, ksamp. = 1, N. = 390, ni. = rep(5,390), K = 4, hs = hs, rx. = A1C$GROUP-1,
                        fitRx = c(F,T), id = rep(1:390,5), hyperpar= c(5,1,1,1,1,.001, .0002), run. = 1)
